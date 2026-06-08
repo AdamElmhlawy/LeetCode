0c34fdee1180706891e653aa405ae1aac6d08bd7
AdamElmhlawy <adam.elmhlawy.dev@gmail.com>
2026-06-08 09:40:25 +0300

    0238-PRODUCT-OF-ARRAY-EXCEPT-SELF Stats: Time: 59 ms (9.47%), Space: 26.8 MB (5.44%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+
+        l_multi = 1
+        r_multi = 1
+        l_arr = [0] * n
+        r_arr = [0] * n
+        for i in range(n):
+            j = -i -1
+            l_arr[i] = l_multi
+            r_arr[j] = r_multi
+            l_multi = nums[i] * l_multi
+            r_multi = nums[j] * r_multi
+        return [l * r for l, r in zip(l_arr, r_arr)]
+        
