96ab11bf97b814be56443c3bc8bdac8069088ff1
AdamElmhlawy <adam.elmhlawy.dev@gmail.com>
2026-06-10 07:17:02 +0300

    0217-CONTAINS-DUPLICATE Stats: Time: 12 ms (85.09%), Space: 25.8 MB (56.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,13 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         sett = set(nums)
-        set_nums = []
-        for num in sett:
-            set_nums.append(num)
-        print(set_nums)
-        print(sett)
-        print(nums)
-        if len(set_nums) == len(nums):
+        if len(sett) == len(nums):
             return False
         else:
             return True
