b0c0a6aa26f5ac0d53cbf9a8e7747fbb69247693
AdamElmhlawy <adam.elmhlawy.dev@gmail.com>
2026-06-10 07:35:05 +0300

    0242-VALID-ANAGRAM Stats: Time: 28 ms (10.16%), Space: 13.4 MB (39.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,14 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        l_s = []
-        l_t = []
-        for char in s:
-            l_s.append(char)
-        for char in t:
-            l_t.append(char)
-        
-        if sorted(l_s) == sorted(l_t):
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
